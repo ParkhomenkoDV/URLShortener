dbe40c01e7ba46fb53e78ca6b4267fb56a19dca7
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-10-10 13:50:41 +0300

    fix config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,53 +1,26 @@
 package config
 
-import (
-	"flag"
-	"net/url"
-
-	"github.com/caarlos0/env/v11"
-)
-
-const defaultAddress = "localhost:8080"
-
+// Конфигурация проекта
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS"` // envDefault:"localhost:8080"`
-	BaseURL       string `env:"BASE_URL"`       // envDefault:"http://localhost:8080"`
-	FileStorage   string `env:"FILE_STORAGE_PATH"`
-	DBDSN         string `env:"DATABASE_DSN"`
+	Protocol      string
+	Port          string
+	ShortAddress  string
+	FilePath      string
+	AddressDB     string
+	AuthSecretKey string
 }
 
-func NewConfig() (Config, error) {
-	config := Config{}
+// Инициализация конфигурации
+func New() *Config {
+	// Получение данных из флагов
+	reqAddress, resAddress, filePath, adressDB := parseFlags()
 
-	err := env.Parse(&config)
-	if err != nil {
-		return config, err
+	return &Config{
+		Protocol:      "http://",
+		Port:          reqAddress,
+		ShortAddress:  resAddress,
+		FilePath:      filePath,
+		AddressDB:     adressDB,
+		AuthSecretKey: "your-secret-key-change-in-production", // В продакшене должен быть из переменной окружения
 	}
-
-	configFlags := Config{}
-
-	flag.StringVar(&configFlags.ServerAddress, "a", defaultAddress, "Server address")
-	flag.StringVar(&configFlags.BaseURL, "b", "http://"+defaultAddress, "Base URL")
-	flag.StringVar(&configFlags.FileStorage, "f", "data/db.json", "File Storage")
-	flag.StringVar(&config.DBDSN, "d", config.DBDSN, "DB DSN")
-	flag.Parse()
-
-	if config.ServerAddress == "" {
-		config.ServerAddress = configFlags.ServerAddress
-	}
-	if config.BaseURL == "" {
-		config.BaseURL = configFlags.BaseURL
-	}
-	if config.FileStorage == "" {
-		config.FileStorage = configFlags.FileStorage
-	}
-	if config.DBDSN == "" {
-		config.DBDSN = configFlags.DBDSN
-	}
-
-	if _, err := url.ParseRequestURI(config.BaseURL); err != nil {
-		return config, err
-	}
-
-	return config, nil
 }
